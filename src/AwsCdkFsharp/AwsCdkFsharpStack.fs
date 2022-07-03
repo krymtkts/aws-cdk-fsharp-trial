13a1200a5ca6606ec475797ef8bf5d4a30cafdc6
krymtkts <takatoshi.kuriyama@gmail.com>
2022-07-03 16:24:03 +0900

    Write a stack.

--- src/AwsCdkFsharp/AwsCdkFsharpStack.fs
+++ src/AwsCdkFsharp/AwsCdkFsharpStack.fs
@@ -1,14 +1,60 @@
 namespace AwsCdkFsharp
 
 open Amazon.CDK
-open Amazon.CDK.AWS.SNS
-open Amazon.CDK.AWS.SNS.Subscriptions
-open Amazon.CDK.AWS.SQS
+open Amazon.CDK.AWS.S3
+open Amazon.CDK.AWS.Lambda
+open Amazon.CDK.AWS.Logs
+open Amazon.CDK.AWS.Events
+open System.Collections.Generic
+open Amazon.CDK.AWS.Events.Targets
 
 type AwsCdkFsharpStack(scope, id, props) as this =
     inherit Stack(scope, id, props)
 
-    let queue = Queue(this, "AwsCdkFsharpQueue", QueueProps(VisibilityTimeout = Duration.Seconds(300.)))
+    let bucket =
+        Bucket(
+            this,
+            "source-bucket",
+            BucketProps(
+                BucketName = "source-bucket",
+                Versioned = false,
+                RemovalPolicy = RemovalPolicy.DESTROY,
+                AutoDeleteObjects = true
+            )
+        )
 
-    let topic = Topic(this, "AwsCdkFsharpTopic")
-    do topic.AddSubscription(SqsSubscription(queue))
+    let lfunc =
+        Function(
+            this,
+            "sample-function",
+            FunctionProps(
+                FunctionName = "SampleFunction",
+                Description = "sample function.",
+                Runtime = Runtime.DOTNET_6,
+                Code = Code.FromAsset("./src/api/bin/"),
+                Handler = "api::api.Function::HunctionHandler",
+                Architecture = Architecture.ARM_64,
+                MemorySize = (Some 128.0 |> Option.toNullable),
+                Timeout = Duration.Seconds(10),
+                LogRetention = (Some RetentionDays.ONE_DAY |> Option.toNullable)
+            )
+        )
+
+    let key: IDictionary<string, obj> = dict [ "prefix", "/" ]
+
+    let detail: IDictionary<string, obj> =
+        dict [ "bucket", dict [ "name", [| bucket.BucketName |] ]
+               "object", dict [ "key", [| key |] ] ]
+
+    do
+        Rule(
+            this,
+            "bucket-event",
+            RuleProps(
+                RuleName = "buckt-event",
+                Description = "bucket event.",
+                EventPattern =
+                    EventPattern(Source = [| "aws.s3" |], DetailType = [| "Object Created" |], Detail = detail)
+            )
+        )
+            .AddTarget(LambdaFunction(lfunc))
